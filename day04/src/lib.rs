455a7bd3e0fcdb686506c833dab6c790c7257291
Mark Day <mday@mac.com>
2025-12-04 10:05:15 -0700

    Day 4, part 2

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -17,8 +17,40 @@ pub fn part1(input: &str) -> usize {
     }).count()
 }
 
-pub fn part2(_input: &str) -> String {
-    "World".to_string()
+pub fn part2(input: &str) -> usize {
+    let mut rolls: HashSet<(isize, isize)> = HashSet::new();
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch == '@' {
+                rolls.insert((row as isize, col as isize));
+            }
+        }
+    }
+    let initial_count = rolls.len();
+
+    loop {
+        // TODO: Rather than considering all rolls, could we just consider
+        // neighbors of just-removed rolls?
+        let removable: HashSet<(isize, isize)> = rolls.iter().filter(|(row, col)| {
+            let num_neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+                .iter().filter(|(dr, dc)| rolls.contains(&(row + dr, col + dc))).count();
+            num_neighbors < 4
+        }).cloned().collect();
+
+        if removable.is_empty() {
+            break;
+        }
+
+        eprintln!("Removing {} rolls", removable.len());
+
+        for roll in removable {
+            rolls.remove(&roll);
+        }
+    }
+
+    eprintln!("{} rolls remaining", rolls.len());
+
+    initial_count - rolls.len()
 }
 
 #[cfg(test)]
@@ -51,6 +83,11 @@ mod tests {
 
     #[test]
     fn test_part2_example() {
-        assert_eq!(part2(EXAMPLE_INPUT), "World");
+        assert_eq!(part2(EXAMPLE_INPUT), 43);
+    }
+
+    #[test]
+    fn test_part2_full() {
+        assert_eq!(part2(FULL_INPUT), 8739);
     }
 }
