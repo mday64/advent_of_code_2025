7e26f4a39a80f9fb0a199094341bd03a9c05b4b4
Mark Day <mday@mac.com>
2025-12-05 10:12:28 -0700

    Day 5, part 1

---
+++ day05/src/lib.rs
@@ -0,0 +1,71 @@
+use std::ops::RangeInclusive;
+use nom::{IResult, Parser, bytes::complete::tag, character::complete::{newline, u64}, combinator::all_consuming, multi::many1, sequence::{separated_pair, terminated}};
+
+pub fn part1(input: &str) -> usize {
+    let (ranges, ids) = parse_input(input);
+    ids.into_iter()
+        .filter(|id| ranges.iter().any(|range| range.contains(id)))
+        .count()
+}
+
+pub fn part2(_input: &str) -> String {
+    "World".to_string()
+}
+
+fn parse_input(input: &str) -> (Vec<RangeInclusive<u64>>, Vec<u64>) {
+    let (_, (ranges, ids)) = all_consuming(
+        separated_pair(parse_ranges, newline, parse_ids)
+    ).parse(input)
+    .expect("Invalid input");
+
+    (ranges, ids)
+}
+
+fn parse_range(input: &str) -> IResult<&str, RangeInclusive<u64>> {
+    terminated(separated_pair(u64, tag("-"), u64), newline)
+        .map(|(first, second)| first..=second)
+        .parse(input)
+}
+
+fn parse_ranges(input: &str) -> IResult<&str, Vec<RangeInclusive<u64>>> {
+    many1(parse_range).parse(input)
+}
+
+fn parse_ids(input: &str) -> IResult<&str, Vec<u64>> {
+    many1(terminated(u64, newline)).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+    
+    static EXAMPLE_INPUT: &str = "\
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
+    static FULL_INPUT: &str = include_str!("../input.txt");
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(part1(EXAMPLE_INPUT), 3);
+    }
+
+    #[test]
+    fn test_part1_full() {
+        assert_eq!(part1(FULL_INPUT), 874);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(part2(EXAMPLE_INPUT), "World");
+    }
+}
