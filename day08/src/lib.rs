db30d58777783dc8ce21f2d2e8b3538b8585cf6a
Mark Day <mday@mac.com>
2025-12-08 14:10:04 -0700

    Day 8

---
+++ day08/src/lib.rs
@@ -0,0 +1,161 @@
+use std::collections::HashSet;
+use itertools::Itertools;
+use parsing::parse_input;
+
+#[derive(Hash, PartialEq, Eq)]
+struct Point {
+    x: u64,
+    y: u64,
+    z: u64
+}
+
+impl std::fmt::Debug for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Point(x: {}, y: {}, z: {})", self.x, self.y, self.z)
+    }
+}
+
+impl Point {
+    // Return the square of the Euclidean distance from self to other
+    fn distance_to(&self, other: &Self) -> u64 {
+        self.x.abs_diff(other.x).pow(2) +
+        self.y.abs_diff(other.y).pow(2) +
+        self.z.abs_diff(other.z).pow(2)
+    }
+}
+
+pub fn part1(input: &str, num_connections: usize) -> usize {
+    // Parse the input
+    let points = parse_input(input);
+
+    // Produce a list of all unique pairs of points, sorted by
+    // distance between the points.  Sorted from largest distance
+    // to smallest distance, so that the smallest can be .pop()'ed.
+    let mut pairs = points.iter().combinations(2).collect_vec();
+    pairs.sort_unstable_by_key(|pair| pair[0].distance_to(&pair[1]));
+    pairs.reverse();
+
+    // Create a list/set of components (circuits).
+    // Initially, each point is in its own separate component.
+    let mut circuits = points.iter().map(|point| HashSet::from([point])).collect_vec();
+
+    for _ in 0..num_connections {
+        // Find and remove the shortest remaining distance -> two points
+        let pair = pairs.pop().unwrap();
+        // dbg!(&pair);
+
+        // If the two points are in different circuits, then connect them.
+        // The hard part here is getting mutable refences to both circuits
+        // at the same time.
+        let mut indices = pair.iter().map(|point|
+            circuits.iter().position(|circuit| circuit.contains(point)).unwrap()
+        ).collect_vec();
+        if indices[0] != indices[1] {
+            indices.sort_unstable();
+            // dbg!(&indices);
+            let (part1, part2) = circuits.split_at_mut(indices[1]);
+            part1[indices[0]].extend(part2[0].drain());
+            circuits.remove(indices[1]);
+            // dbg!(&circuits);
+        }
+
+    }
+
+    // Sort components/circuits by number of points
+    // Return the sum of the three largest sizes
+    let mut lengths = circuits.iter().map(|circuit| circuit.len()).collect_vec();
+    lengths.sort_unstable();
+    // dbg!(&lengths);
+    lengths.iter().rev().take(3).product()
+}
+
+pub fn part2(input: &str) -> u64 {
+    // Parse the input
+    let points = parse_input(input);
+
+    // Produce a list of all unique pairs of points, sorted by
+    // distance between the points.  Sorted from largest distance
+    // to smallest distance, so that the smallest can be .pop()'ed.
+    let mut pairs = points.iter().combinations(2).collect_vec();
+    pairs.sort_unstable_by_key(|pair| pair[0].distance_to(&pair[1]));
+    pairs.reverse();
+
+    // Create a list/set of components (circuits).
+    // Initially, each point is in its own separate component.
+    let mut circuits = points.iter().map(|point| HashSet::from([point])).collect_vec();
+
+    loop {
+        // Find and remove the shortest remaining distance -> two points
+        let pair = pairs.pop().unwrap();
+        // dbg!(&pair);
+
+        // If the two points are in different circuits, then connect them.
+        // The hard part here is getting mutable refences to both circuits
+        // at the same time.
+        let mut indices = pair.iter().map(|point|
+            circuits.iter().position(|circuit| circuit.contains(point)).unwrap()
+        ).collect_vec();
+        if indices[0] != indices[1] {
+            if circuits.len() == 2 {
+                return pair[0].x * pair[1].x;
+            }
+            indices.sort_unstable();
+            // dbg!(&indices);
+            let (part1, part2) = circuits.split_at_mut(indices[1]);
+            part1[indices[0]].extend(part2[0].drain());
+            circuits.remove(indices[1]);
+            // dbg!(&circuits);
+        }
+
+    }
+}
+
+mod parsing {
+    use super::Point;
+    use nom::{IResult, Parser, character::complete::{char, newline, u64}, combinator::all_consuming, multi::many1, sequence::terminated};
+
+    fn point(input: &str) -> IResult<&str, Point> {
+        let (remaining, (x, y, z)) = (
+            terminated(u64, char(',')),
+            terminated(u64, char(',')),
+            terminated(u64, newline)
+        ).parse(input)?;
+
+        Ok((remaining, Point{x, y, z}))
+    }
+
+    pub fn parse_input(input: &str) -> Vec<Point> {
+        let (_, points) = all_consuming(many1(point))
+            .parse(input)
+            .expect("Invalid input");
+        points
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+    
+    static EXAMPLE_INPUT: &str = include_str!("../example.txt");
+    static FULL_INPUT: &str = include_str!("../input.txt");
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(part1(EXAMPLE_INPUT, 10), 40);
+    }
+
+    #[test]
+    fn test_part1_full() {
+        assert_eq!(part1(FULL_INPUT, 1000), 127551);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(part2(EXAMPLE_INPUT), 25272);
+    }
+
+    #[test]
+    fn test_part2_full() {
+        assert_eq!(part2(FULL_INPUT), 2347225200);
+    }
+}
---
+++ day08/src/main.rs
@@ -0,0 +1,11 @@
+use day08::{part1, part2};
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let result1 = part1(input, 1000);
+    println!("Part 1: {result1}");
+
+    let result2 = part2(input);
+    println!("Part 2: {result2}");
+}
