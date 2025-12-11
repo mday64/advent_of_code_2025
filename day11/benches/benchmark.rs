cc027637446ffb9f7a92b2f6c9fa14fc3691a8f6
Mark Day <mday@mac.com>
2025-12-11 10:34:33 -0700

    Day 11, part 1

---
+++ day11/benches/benchmark.rs
@@ -0,0 +1,17 @@
+use day11::{ part1, part2 };
+
+fn main() {
+    divan::main();
+}
+
+const INPUT: &str = include_str!("../input.txt");
+
+#[divan::bench]
+fn bench_part1() {
+    part1(INPUT);
+}
+
+#[divan::bench]
+fn bench_part2() {
+    part2(INPUT);
+}
---
+++ day11/src/lib.rs
@@ -0,0 +1,74 @@
+use rustc_hash::FxHashMap;
+
+//
+// Find the number of distinct paths from node "you" to node "out".
+//
+// NOTE: I can't use pathfinding::directed::dfs::dfs_reach because it
+// caches the nodes it has visited, and would only return "out" once.
+pub fn part1(input: &str) -> u32 {
+    let graph = parse_input(input);
+    let mut result = 0;
+    // This feels like I'm declaring the closure wrong.  I shouldn't need &mut.
+    dfs("you", &graph, &mut |node| {
+        if node == "out" {
+            result += 1;
+        }
+    });
+    result
+}
+
+pub fn part2(_input: &str) -> String {
+    "World".to_string()
+}
+
+fn dfs<T: FnMut(&str)>(start: &str, graph: &FxHashMap<&str, Vec<&str>>, visit: &mut T) {
+    visit(start);
+    for &neighbor in graph[start].iter() {
+        dfs(neighbor, graph, visit);
+    }
+}
+
+fn parse_input(input: &str) -> FxHashMap<&str, Vec<&str>> {
+    let mut result: FxHashMap<&str, Vec<&str>> = input.lines()
+    .map(|line| {
+        let (node, rest) = line.split_once(": ").unwrap();
+        let neighbors = rest.split_ascii_whitespace().collect();
+        (node, neighbors)
+    }).collect();
+    result.insert("out", vec![]);
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+    
+    static EXAMPLE_INPUT: &str = "\
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+";
+    static FULL_INPUT: &str = include_str!("../input.txt");
+
+    #[test]
+    fn test_part1_example() {
+        assert_eq!(part1(EXAMPLE_INPUT), 5);
+    }
+
+    #[test]
+    fn test_part1_full() {
+        assert_eq!(part1(FULL_INPUT), 643);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        assert_eq!(part2(EXAMPLE_INPUT), "World");
+    }
+}
